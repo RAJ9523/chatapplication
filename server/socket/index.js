f33335633eb57ddef60f12d4dc6c3b2bb923bc75
Ronit9523 <rajronit813@gmail.com>
2024-07-26 21:37:30 +0530

    init

--- server/socket/index.js
+++ server/socket/index.js
@@ -20,7 +20,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors : {
-        origin :process.env.FRONTEND_URL,
+        origin :true,
         credentials : true
     }
 })
